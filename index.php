24ee1fa31204f8b3ff2187428a66aa9a70b2f931
JansonYu <yujun_win@163.com>
2016-10-26 15:35:03 +0800

    Add files via upload
    index

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+	<meta charset="UTF-8">
+
+	<title>柏富毛纺</title>
+
+	<link rel="stylesheet" href="./css/slide.css">
+
+	<link rel="stylesheet" href="./css/base.css">
+
+	<!-- <link rel="stylesheet" href="./css/base.min.css"> -->
+
+	<script src="./js/jquery1.7.1.js"></script>
+
+	<script src="./js/jquery.event.swipe.js"></script>
+
+	<script src="./js/jquery.event.move.js"></script>
+
+	<script src="./js/unslider.min.js"></script>
+
+</head>
+
+<body>
+<?php include 'block/header.php'?>
+
+
+<div class="banner">
+
+	<ul>
+
+		<li style="background: url(imgs/banner1.jpg) no-repeat 50% 0 ;">
+
+			<div class="inner">
+
+				<span class="white">柏富故事，历久弥新</span>
+
+				<span class="black">创于1988年，雄厚工艺基础及先进设备</span>
+
+				<p class="info"><a href="about.php" class="go">了解更多></a></p>
+
+			</div>
+
+		</li>
+
+		<li style="background: url(imgs/banner2.jpg) no-repeat 50% 0 ;"></li>
+
+		<li style="background: url(imgs/banner3.jpg) no-repeat 50% 0 ;"></li>
+
+	</ul>
+
+</div>
+
+<div class="unit com-info">
+
+	<div class="inner">
+
+		<div class="l">
+
+			<p class="title">公司简介</p>
+
+			<hr>
+
+			<p>
+
+				绍兴市柏富毛纺织有限公司是一家专业生产针织纱的毛纺企业。公司始创于1988年，拥有完善的管理制度、雄厚的工艺基础，并在此基础上锐意创新，引进<a href="workshop.php">国外先进设备</a>——<a href="workshop.php">意大利精纺络筒机</a>、<a href="workshop.php#w2">意大利走锭机</a>、<a href="workshop.php#w2">意大利梳毛机</a>、<a href="workshop.php#w3">法国粗纱机</a>、<a href="workshop.php#w3">日本拼线机</a>、<a href="workshop.php#w3">德国倍捻机</a>等。现有精纺纱锭8200锭、粗纺纱锭3600锭、半精纺纱锭5000锭、4台套走锭生产线。公司年生产能力为精纺1000吨，粗纺纱600吨，半精纺600吨。公司精于生产各类16S—48S粗纺走锭纱、兔毛混纺纱、羊仔纱、羊绒纱、羊绒混纺纱、20S—100S精纺纱、全羊毛纱系列、丝光防缩羊毛系列、天丝羊毛系列、
+
+	莫代尔羊毛系列……
+
+			</p><a href="about.php" class="a_button">了解更多</a>
+
+			</div>
+
+			<div class="r">
+				<a href="javascript:;" class="a prev"></a>
+				<a href="javascript:;" class="a next"></a>
+				<div class="nt-container">
+				<div class="nt">
+
+					<a href="./product-basulan.php"><img src="imgs/inder-r-1.png" alt="柏富毛纺"></a>
+
+					<a href="./product-basulan.php" class="title">巴素兰毛纱</a>
+
+					<p class="msg">巴素兰（Basolan）毛纱采用高支美丽诺羊毛，经过德国巴斯夫公司的巴素兰处理技术处理，使羊毛磷片钝化，而不是简单剥除羊毛磷片的办法。</p>
+
+				</div>
+
+				<div class="nt">
+
+					<a href="./product-meilinuo.php"><img src="imgs/inder-r-2.png" alt="柏富毛纺"></a>
+
+					<a href="./product-meilinuo.php" class="title">美利奴毛纱 </a>
+
+					<p class="msg">美利奴毛纱进口澳大利亚美利奴羊原毛，毛条生产、染色、纺纱一条龙自主完成，产品生产周期短，质量稳定。羊毛纱线种类齐全，广泛用于各个领域。</p>
+
+				</div>
+
+				<div class="nt">
+
+					<a href="./product-maoni.php"><img src="imgs/inder-r-4.png" alt="柏富毛纺"></a>
+
+					<a href="./product-maoni.php" class="title" style="padding-top:53px">羊毛呢料 </a>
+
+					<p class="msg">采用优质的山羊绒、羊毛、兔毛等天然面料制作。其中，高档双面呢由两层独立面料梭织而成，因此相较于单层的毛呢，双面呢面料更加保暖。</p>
+
+				</div>
+
+				<div class="nt">
+
+					<a href="./product-turong.php"><img src="imgs/inder-r-3.png" alt="柏富毛纺"></a>
+
+					<a href="./product-turong.php" class="title" style="padding-top:53px">兔绒混纺纱系列 </a>
+
+					<p class="msg">采用优质的长毛兔绒、山羊绒、羊毛等混纺。其特点为纤维细，手感滑糯、表面绒毛飘拂、色泽柔和、蓬松性好。</p>
+
+				</div>
+				</div>
+
+			</div>
+
+	</div>
+
+</div>
+
+<div class="unit">
+
+	<div class="inner">
+
+		<p class="title">设备设施</p>
+
+		<hr>
+
+		<div class="card first">
+
+			<div class="in">
+				<a href="workshop.php"><img src="imgs/index-ss1.png" alt="柏富毛纺"></a>
+
+				<p class="title">精纺车间</p>
+
+				<p class="msg">意大利精纺络筒机，日本拼线机、德国倍捻机。现有精纺纱锭8200锭，公司年生产能力为精纺1000吨  <a href="workshop.php" class="go">more></a></p>
+			</div>
+
+		</div>
+
+		<div class="card second">
+
+			<div class="in">
+				<a href="workshop.php#w2"><img src="imgs/index-ss2.png" alt="柏富毛纺"></a>
+
+				<p class="title">半精纺车间</p>
+
+				<p class="msg">日本梳棉机、进口细纱机，德国倍捻机。现有半精纺纱锭5000锭，公司年生产能力为半精纺600吨 <a href="workshop.php#w2" class="go">more></a></p>
+			</div>
+
+		</div>
+
+		<div class="card third">
+
+			<div class="in">
+				<a href="workshop.php#w3"><img src="imgs/index-ss1.png" alt="柏富毛纺"></a>
+
+				<p class="title">粗纺车间</p>
+
+				<p class="msg">法国粗纱机，意大利走锭机、意大利梳毛机。现有粗纺纱锭3600锭，公司年生产能力为精纺1000吨 <a href="workshop.php#w3" class="go">more></a></p>
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+<?php include 'block/footer.php'?>
+
+<script>
+
+    $('.banner').unslider({
+
+				arrows: true,
+
+				fluid: true,
+
+				dots: true
+
+			});
+
+    $('.a.next').on('click',function() {
+    	var _left = parseInt($('.nt-container').css('left'))-275,
+    		$node = $('.nt-container'),
+    		$last = $node.find('.nt').last(),
+    		$clone = $last.clone(true);
+    	$node.prepend($clone);
+    	$last.remove();
+    	$node.css({'left':_left})
+    	$node.animate({'left':_left+275})
+    })
+    $('.a.prev').on('click',function() {
+    	var _left = parseInt($('.nt-container').css('left')) -275 ,
+    		$node = $('.nt-container'),
+    		$first = $node.find('.nt').first(),
+    		$clone = $first.clone(true);
+
+    	$node.append($clone);
+    	$node.animate({'left':_left},function ()
+    	{
+    		$first.remove();
+			$node.css({'left':_left+275});
+    	});	
+		
+			
+	
+    })
+
+  </script>
+
+</body>
+
+</html>
